9e7ec51c19dad782b19865c61615b05b847cf9cb
dan hassin <danhassin@mac.com>
2021-04-01 18:28:15 -0400

    correctly flag await inside catch or finally

--- tests/lib/rules/handled-await.js
+++ tests/lib/rules/handled-await.js
@@ -8,30 +8,28 @@ RuleTester.setDefaultConfig({
   }
 });
 
+const errors = [{
+  message: "await expression does not .catch() and is not in a try block",
+  type: "AwaitExpression"
+}];
+
 const ruleTester = new RuleTester();
 ruleTester.run("handled-await", rule, {
   valid: [
     "async () => { try { await foo(); } catch (e) {} }",
-    "async () => { try { await foo(); } finally {} }",
+    "async () => { try {} catch (e) { await foo().catch() } }",
+    "async () => { try {} catch (e) { try { await foo() } catch (e) {} } }",
+    "async () => { try { await foo(); } finally { await foo().catch(e) } }",
+    "async () => { try { await foo(); } finally { try { await foo() } catch (e) {} } }",
     "async () => { await foo().catch(); }",
     "async () => { await foo.catch(); }",
     "async () => { await foo.bar().catch(); }",
   ],
 
   invalid: [
-    {
-      code: "async function a() { await foo(); }",
-      errors: [{
-        message: "await expression does not .catch() and is not in a try-catch block",
-        type: "AwaitExpression"
-      }]
-    },
-    {
-      code: "async () => { await foo().catch().then(); }",
-      errors: [{
-        message: "await expression does not .catch() and is not in a try-catch block",
-        type: "AwaitExpression"
-      }]
-    }
+    { code: "async function a() { await foo(); }", errors },
+    { code: "async () => { await foo().catch().then(); }", errors },
+    { code: "async () => { try {} catch (e) { await foo(); } }", errors },
+    { code: "async () => { try {} finally { await foo(); } }", errors },
   ]
 });
